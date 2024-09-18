e6d6ac39ea569b71e45fc55547b6d910c526ebdd
Lane Enget <enget.lane@gmail.com>
2024-09-17 22:01:26 -0500

    Start matrix3

---
+++ src/math/Matrix3.cs
@@ -0,0 +1,15 @@
+namespace Jinx.Src.Math {
+
+    public class Matrix3{
+
+        public static readonly Matrix3 IDENTITY = new Matrix3();
+        public List<float> mat = new List<float>(new float[9]);
+
+        public static Matrix3 Multiply(Matrix3 m1, Matrix3 m2) {
+
+            var m = new Matrix3();
+
+            m.mat[0] = 
+        }
+    }
+}
